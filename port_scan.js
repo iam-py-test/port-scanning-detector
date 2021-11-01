6da3d2d5005fbaa47dc97171f5c4bdb11b3485c5
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-10-31 20:10:53 -0400

    I finaly got around to making this

---
+++ port_scan.js
@@ -0,0 +1,11 @@
+var http = require("http")
+var port =  40 //a port you want to protect
+http.createServer(function(req,res){
+console.log("Attack detected on port " + port)
+console.log("URL requested: " + req.url)
+console.log("User agent string: " + req.headers["user-agent"])
+console.log("Host header: " + req["headers"].host)
+res.writeHead(200,{"Content-Type":"text/plain"})
+res.write("000")
+res.end()
+}).listen(port)
